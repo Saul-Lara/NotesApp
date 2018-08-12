7175db4d57978aeca874046f5eeaf7e2cdd9dcb0
Saul Hdz Lara <33563111+Saul-Lara@users.noreply.github.com>
2018-08-12 03:42:49 -0500

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,8 +1,3 @@
 export const DB_CONFIG = {
-    apiKey: "AIzaSyB0gxnwElpMm9oJ35fR0rXJN6nLtJM49aw",
-    authDomain: "notesappreact.firebaseapp.com",
-    databaseURL: "https://notesappreact.firebaseio.com",
-    projectId: "notesappreact",
-    storageBucket: "notesappreact.appspot.com",
-    messagingSenderId: "710086118933"
+    
 }
